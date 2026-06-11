5c0fffae476019dbdc965ca5decda21405dfb831
John31415 <johnmauris04@gmail.com>
2026-06-11 04:27:23 -0400

    feat(llm): update llm communication

--- src/llm/ask_llm.py
+++ src/llm/ask_llm.py
@@ -1,4 +1,3 @@
-from src.llm.prompt import create_prompt
 import os
 from dotenv import load_dotenv
 from pathlib import Path
@@ -6,19 +5,18 @@ import os
 from cloudflare import Cloudflare
 
 
-def ask_llm(query: str) -> str:
+def ask_llm(system_prompt: str, user_prompt: str) -> str:
     env_path = Path(__file__).resolve().parent.parent.parent / ".env"
     load_dotenv(dotenv_path=env_path)
-    prompt_system, prompt_user = create_prompt(query)
     client = Cloudflare(api_token=os.environ.get("CF_TOKEN"))
     account_id = os.environ.get("CF_ID")
     response = client.ai.run(
         account_id=account_id,
         model_name="@cf/meta/llama-3.1-8b-instruct",
         messages=[
-            {"role": "system", "content": prompt_system},
-            {"role": "user", "content": prompt_user},
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
         ],
-        max_tokens=512,
+        max_tokens=1024,
     )
     return response["response"]
--- src/llm/prompt.py
+++
@@ -1,8 +0,0 @@
-def create_prompt(query: str) -> tuple[str, str]:
-    prompt_system = """
-
-    """
-    prompt_user = f"""
-
-    """
-    return (prompt_system, prompt_user)
